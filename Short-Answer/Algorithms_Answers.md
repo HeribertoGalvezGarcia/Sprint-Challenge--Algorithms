313655be758bfb961b617e43154dfd91923d996a
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-06-19 10:56:20 -0700

    Finish Sprint

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -4,12 +4,20 @@
 
 a)
 
+O(n). It runs once for each value in n since even though the while is `a < n * n * n` a is `a = a + n * n` so the first two ns get canceled out.
 
 b)
 
+O(n log n). The number if iterations increases each faster than n increases but it increases slower than O(n^2)
 
 c)
 
+O(n). The recursion happens once for every bunny given.
+
 ## Exercise II
 
-
+Start of by dropping egg on the middle floor.
+If it breaks go to floor below to see if it breaks, if it breaks return above floor else set ceiling to current floor.
+Else set base to current floor.
+Repeat until found.
+O(log n)
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -1,55 +1,66 @@
-class SortingRobot:
-    def __init__(self, l):
+from typing import List, Generic, TypeVar, Optional
+
+T = TypeVar('T')
+
+
+class SortingRobot(Generic[T]):
+    _list: List[T]
+    _item: Optional[T]
+    _position: int
+    _light: str
+    _time: int
+
+    def __init__(self, list_: List[T]) -> None:
         """
         SortingRobot takes a list and sorts it.
         """
-        self._list = l          # The list the robot is tasked with sorting
+        self._list = list_      # The list the robot is tasked with sorting
         self._item = None       # The item the robot is holding
         self._position = 0      # The list position the robot is at
-        self._light = "OFF"     # The state of the robot's light
+        self._light = 'OFF'     # The state of the robot's light
         self._time = 0          # A time counter (stretch)
 
-    def can_move_right(self):
+    def can_move_right(self) -> bool:
         """
         Returns True if the robot can move right or False if it's
         at the end of the list.
         """
         return self._position < len(self._list) - 1
 
-    def can_move_left(self):
+    def can_move_left(self) -> bool:
         """
         Returns True if the robot can move left or False if it's
         at the start of the list.
         """
         return self._position > 0
 
-    def move_right(self):
+    def move_right(self) -> bool:
         """
         If the robot can move to the right, it moves to the right and
         returns True. Otherwise, it stays in place and returns False.
         This will increment the time counter by 1.
         """
         self._time += 1
-        if self._position < len(self._list) - 1:
+        if self.can_move_right():
             self._position += 1
             return True
         else:
             return False
 
-    def move_left(self):
+    def move_left(self) -> bool:
         """
         If the robot can move to the left, it moves to the left and
         returns True. Otherwise, it stays in place and returns False.
         This will increment the time counter by 1.
         """
         self._time += 1
-        if self._position > 0:
+        if self.can_move_left():
             self._position -= 1
             return True
         else:
             return False
 
-    def swap_item(self):
+    def swap_item(self) -> None:
         """
         The robot swaps its currently held item with the list item in front
         of it.
@@ -59,7 +70,7 @@ class SortingRobot:
         # Swap the held item with the list item at the robot's position
         self._item, self._list[self._position] = self._list[self._position], self._item
 
-    def compare_item(self):
+    def compare_item(self) -> Optional[int]:
         """
         Compare the held item with the item in front of the robot:
         If the held item's value is greater, return 1.
@@ -76,37 +87,63 @@ class SortingRobot:
         else:
             return 0
 
-    def set_light_on(self):
+    def set_light_on(self) -> None:
         """
         Turn on the robot's light
         """
-        self._light = "ON"
-    def set_light_off(self):
+        self._light = 'ON'
+
+    def set_light_off(self) -> None:
         """
         Turn off the robot's light
         """
-        self._light = "OFF"
-    def light_is_on(self):
+        self._light = 'OFF'
+
+    def light_is_on(self) -> bool:
         """
         Returns True if the robot's light is on and False otherwise.
         """
-        return self._light == "ON"
+        return self._light == 'ON'
 
     def sort(self):
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+
+        while self.can_move_right():
+            self.move_right()
+            self.swap_item()
+
+            while self.can_move_left():
+                self.move_left()
+
+                if self.compare_item() == -1:
+                    continue
+                else:
+                    self.move_right()
+                    break
+
+            self.swap_item()
+
+            while not self.compare_item() is None:
+                self.move_right()
+                self.swap_item()
 
 
-if __name__ == "__main__":
+def main() -> None:
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    list_ = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94,
+             99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85,
+             27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63,
+             31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
-    robot = SortingRobot(l)
+    robot = SortingRobot(list_)
 
     robot.sort()
     print(robot._list)
+
+
+if __name__ == "__main__":
+    main()
